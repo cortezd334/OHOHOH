42c0d9b2f88c8c0401397ca642045ac74652c42e
dbpatnodee <dbpatnode@hotmail.com>
2020-09-06 15:23:06 -0700

    sort of working form, not really

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,24 @@
+let addPet = false
 const collection = document.querySelector('#pet-collection')
 const header = document.getElementById('pet-header')
+const newPetForm = document.querySelector('.add-pet-form')
+
+document.addEventListener("DOMContentLoaded", () => {
+    const addBtn = document.querySelector("#new-pet-btn")
+    const petFormContainer = document.querySelector(".container")
+    addBtn.addEventListener("click", () => {
+      // hide & seek with the form
+      addPet = !addPet
+      if (addPet) {
+        petFormContainer.style.display = "block"
+      } else {
+        petFormContainer.style.display = "none"
+      }
+    })
+  })
+
+  newPetForm.addEventListener('submit', (e) => appendPet(e))
+
 
 function fetchAgency() {
     fetch('http://localhost:3000/agencies/1')
@@ -81,8 +100,97 @@ const renderUserProfile = (user) => {
         <p>${preference}</p>
     </div>
     `
-    
 }
+
+function newPetForm {
+    collection.innerHTML =
+    `<form class="add-pet-form">
+    <h3>Add a pet for adoption</h3> 
+
+    <input
+        type="text"
+        name="name"
+        value=""
+        placeholder="Enter pet's name..."
+        class="input-text"
+        />
+        <br />
+        <input
+        type="text"
+        name="image"
+        value=""
+        placeholder="Enter pets's image URL..."
+        class="input-text"
+        />
+        <br />
+        <input
+        type="text"
+        name="species"
+        value=""
+        placeholder="Enter species..."
+        class="input-text"
+        />
+        <br />
+        <input
+        type="integer"
+        name="age"
+        value=""
+        placeholder="Enter age..."
+        class="input-text"
+        />
+        <br />
+        <input
+        type="text"
+        name="bio"
+        value=""
+        placeholder="Enter pet description..."
+        class="input-text"
+        />
+        <br />
+
+        <input
+        type="submit"
+        name="submit"
+        value="Add Pet to Adoption List"
+        class="submit"
+        />
+    </form> 
+    </div> 
+    <p style="text-align:center">
+    <button id="new-pet-btn">Add Pet For adoption</button>
+    </p>`
+}
+
+const addNewPet = (e) => {
+    e.preventDefault()
+// debugger
+    newPetName = e.target.name.value
+    newPetImage = e.target.image.value
+    newPetSpecies = e.target.species.value
+    newPetAge = e.target.age.value
+    newPetBio = e.target.bio.value
+
+    let newPetInfo = {
+        name: newPetName,
+        image: newPetImage,
+        species: newPetSpecies,
+        age: newPetAge,
+        bio: newPetBio
+        // available: true
+    }
+// let newPetName = 
+
+    fetch('http://localhost:3000/pets', { 
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json',
+        Accept: 'application/json'
+        },
+        body: JSON.stringify(newPetInfo)
+    })
+    .then(resp => resp.json())
+    .then(pet => {appendPet(pet)})
+}
+addNewPet()
 fetchAgency()
 fetchGetPets()
 fetchUser()
